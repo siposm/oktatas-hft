4291d7f40d63035433ac85625dd07d1187d751d1
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2019-07-01 15:36:47 +0200

    separate files

---
+++ LA-04-hazifeladat/hazifeladat/Avenger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hazifeladat
+{
+    class Avenger
+    {
+        public string Name { get; set; }
+
+        [SavedLives("5%", 30)]
+        public int SavedQuantity { get; set; }
+
+        [Avenger(CurrentLocation.Titan)]
+        public void Fight()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+
+
+
+    class NotAvenger
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+
+}
