10534e8c06f63eb773d623243579d65c4b178170
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2019-07-01 19:03:08 +0200

    LA04 hw 2

---
+++ LA-04-hazifeladat-2/lancoltlistas_feladat/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lancoltlistas_feladat
+{
+    class LancoltLista<T>
+    {
+        public LancoltLista()
+        {
+            TulcsordulasiTerulet = new List<T>();
+            fej = null;
+        }
+
+        private ListaElem fej;
+
+        public List<T> TulcsordulasiTerulet { get; set; }
+
+        class ListaElem
+        {
+            public T tartalom;
+            public ListaElem kovetkezo;
+        }
+
+        public void Beszuras(T ujTartalom)
+        {
+            if (ujTartalom.GetType().GetMethods().Where(x => x.Name.Equals("Ugrik")).Any())
+            {
+                ElejereBeszuras(ujTartalom);
+            }
+            else if (ujTartalom.GetType().GetMethods().Where(x => x.Name.Equals("Nyavog")).Any())
+            {
+                VegereBeszuras(ujTartalom);
+            }
+            else
+            {
+                TulcsordulasiTerulet.Add(ujTartalom);
+
+                // debug
+                Console.WriteLine("TULCS");
+                Console.WriteLine(ujTartalom);
+            }
+        }
+
+        private void VegereBeszuras(T uj)
+        {
+            // debug
+            Console.WriteLine("VÉGÉRE");
+            Console.WriteLine(uj);
+
+            // todo: beszúrás megírása
+        }
+
+        private void ElejereBeszuras(T uj)
+        {
+            // debug
+            Console.WriteLine("ELEJÉRE");
+            Console.WriteLine(uj);
+
+            // todo: beszúrás megírása
+        }
+    }
+
+    abstract class Allat
+    {
+
+    }
+
+    class Kutya : Allat
+    {
+        public string Nev { get; set; }
+        public int Eletkor { get; set; }
+
+        public void Ugrik()
+        {
+
+        }
+    }
+
+    class Macska : Allat
+    {
+        public string Nev { get; set; }
+        public int EletekSzama { get; set; }
+        public int Eletkor { get; set; }
+
+        public void Nyavog()
+        {
+
+        }
+
+        public void Maszik()
+        {
+
+        }
+    }
+
+    class Liba : Allat
+    {
+        public int Suly { get; set; }
+        public int Eletkor { get; set; }
+
+        public void Lepegetes()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LancoltLista<Allat> list = new LancoltLista<Allat>();
+            list.Beszuras(new Kutya() { Nev = "Bodri" });
+            list.Beszuras(new Macska() { Nev = "Cirmi" });
+            list.Beszuras(new Liba() { Suly = 6 });
+        }
+    }
+}
---
+++ LA-04-hazifeladat-2/lancoltlistas_feladat/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("lancoltlistas_feladat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lancoltlistas_feladat")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eaa5ad4f-4be7-4f6c-a723-73d01d2f5b90")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
