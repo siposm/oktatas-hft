7da61acdb54b21d95a488bd78890ecb92510faf9
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2019-07-03 17:13:00 +0200

    added extra stuff to LA01

---
+++ LA-01-delegates-anonymous-functions/delegate_rendezes/delegate_rendezes/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_rendezes
+{
+
+    interface MyCompareTo <T> : IComparable
+    {
+        Comparison<T> Comparison { get; set; }
+    }
+
+    class Student : MyCompareTo <Student>
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+
+        public Comparison<Student> Comparison { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            return Comparison.Invoke(this, (obj as Student));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // két hallgató összehasonlítása
+
+            Student s1 = new Student() { Name = "A Lajos", ID = 10 };
+            Student s2 = new Student() { Name = "B Béla", ID = 1230 };
+
+            s1.Comparison = new Comparison<Student>((x, y) => x.ID.CompareTo(y.ID));
+            s2.Comparison = new Comparison<Student>((x, y) => x.Name.CompareTo(y.Name));
+
+            Console.WriteLine(s1.CompareTo(s2)); // ID : -1 >> s1 kisebb mint s2
+            Console.WriteLine(s2.CompareTo(s1)); // NÉV : 1 >> s2 nagyobb mint s1
+
+            Console.WriteLine("\n--------\n");
+
+            
+            
+            
+            // -----------------------------------------------------------------
+
+
+
+
+            Student[] studs = new Student[10];
+            Random r = new Random();
+
+            for (int i = 0; i < studs.Length; i++)
+                studs[i] = new Student()
+                {
+                    Name = "Hallgató-" + r.Next(100,1000),
+                    ID = r.Next(10, 100)
+                };
+
+            for (int i = 0; i < studs.Length; i++)
+                Console.WriteLine(studs[i].Name + "\t" + studs[i].ID);
+
+
+            //Array.Sort(studs, new Comparison<Student>((x, y) => x.Name.CompareTo(y.Name)));
+            Array.Sort(studs, new Comparison<Student>((x, y) => x.ID.CompareTo(y.ID)));
+
+
+            Console.WriteLine("\n--------\n");
+
+            for (int i = 0; i < studs.Length; i++)
+                Console.WriteLine(studs[i].Name + "\t" + studs[i].ID);
+        }
+    }
+}
