1ba300f87b81affa90b92e0f76dc0706417aae30
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2019-07-03 11:42:32 +0200

    fixed dll solutions

--- LA-05-dll/CalculatorLibrary/CalculatorLibrary/Calculator.cs
+++ LA-05-dll/CalculatorLibrary/CalculatorLibrary/Calculator.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace my_dll_file
+namespace CalculatorLibrary
 {
     public class Calculator
     {
--- LA-05-dll/dllKezelo/dllKezelo/Properties/AssemblyInfo.cs
+++ LA-05-dll/dllKezelo/dllKezelo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("my-dll-file")]
+[assembly: AssemblyTitle("dllKezelo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("my-dll-file")]
+[assembly: AssemblyProduct("dllKezelo")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("09f05654-cbe2-417c-8cad-e1dc3c4daad3")]
+[assembly: Guid("2fe39c7b-f4ea-4ddc-bec1-74cb329f604c")]
 
 // Version information for an assembly consists of the following four values:
 //
