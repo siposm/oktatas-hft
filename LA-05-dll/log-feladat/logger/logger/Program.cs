ea7328e9eb28bbefb48d1b65178c2323ac9c45a9
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2019-07-03 13:15:31 +0200

    logger for xml added and tested

--- LA-05-dll/log-feladat/logger/logger/Program.cs
+++ LA-05-dll/log-feladat/logger/logger/Program.cs
@@ -4,18 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using LoggerTXT; // >> LoggerForTxt
-using LoggerLibrary; // >> interface + student osztály
+using LoggerTXT;        // >> logger for txt
+using LoggerLibrary;    // >> interface + student osztály
+
+using System.Linq;      // >> xdocument
+using System.Xml.Linq;  // >> xdocument
 
 namespace logger
 {
+    class LoggerForXML : ILogger
+    {
+        public void Log(Student student)
+        {
+            XDocument xdoc = new XDocument();
+            xdoc.Add(
+                new XElement("student_entity",
+                    new XElement("name" , student.Name,
+                        new XAttribute("hash" , student.Name.GetHashCode())),
+                    new XElement("registration", student.RegistrationDate)
+                ));
+            xdoc.Save("output_xml.xml");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            LoggerForTXT logger = new LoggerForTXT();
-            logger.Log(new Student() { Name = "Gipsz Géza ___ " });
+            try
+            {
+                LoggerForTXT txtLogger = new LoggerForTXT();
+                txtLogger.Log(new Student() { Name = "Gipsz Géza" });
 
+                LoggerForXML xmlLogger = new LoggerForXML();
+                xmlLogger.Log(new Student() { Name = "Gipsz Géza" });
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("[ERR] error happened");
+            }
+            finally
+            {
+                Console.WriteLine("ready...");
+            }
         }
     }
 }
