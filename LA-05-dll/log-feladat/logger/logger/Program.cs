defb46c81872fe14471d904d1680d826e98c33f9
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2019-07-03 13:06:35 +0200

    logger dll (class lib), logger main project added

---
+++ LA-05-dll/log-feladat/logger/logger/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LoggerTXT; // >> LoggerForTxt
+using LoggerLibrary; // >> interface + student osztály
+
+namespace logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggerForTXT logger = new LoggerForTXT();
+            logger.Log(new Student() { Name = "Gipsz Géza ___ " });
+
+        }
+    }
+}
