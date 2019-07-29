5bfd39a82c1b3bd542d39a88236d1f218653ead1
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2019-07-29 13:16:45 +0200

    added test cases and classes

---
+++ LA-06-unit-teszt/unittesting/unittesting/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unittesting
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int StartYear { get; set; }
+
+        public void LogStudentToTXT()
+        {
+            StreamWriter sw = new StreamWriter("student.txt");
+            sw.WriteLine(Name);
+            sw.Close();
+        }
+
+        public void CreateInstanceFromString(string input)
+        {            
+            if (input.Contains("#"))
+                throw new FormatException("not valid format");
+            else
+            {
+                // input = [name]%[year]
+
+                this.Name = input.Split('%')[0];
+                this.StartYear = int.Parse(input.Split('%')[1]);
+            }
+        }
+    }
+}
