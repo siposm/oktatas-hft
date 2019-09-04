c045454b08b6e6690c204c4fdd38e78eecee23c9
Miklos Sipos <sipos.miklos@nik.uni-obuda.hu>
2019-09-04 15:16:11 +0200

    fixed typo " line 7

--- get-updated-badge-info.sh
+++ get-updated-badge-info.sh
@@ -4,4 +4,4 @@ echo "collecting stas for badges"
 
 commits=`git rev-list --all --count`
 latest_release_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-echo "{\"commits\":\"$commits\", \"release_tag"\:\"$latest_release_tag\"}" > badges.json
+echo "{\"commits\":\"$commits\", \"release_tag\":\"$latest_release_tag\"}" > badges.json
