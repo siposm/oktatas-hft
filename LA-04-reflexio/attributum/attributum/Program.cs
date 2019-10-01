0caa253ac7e5769c3471c7598342b64c07254fbe
siposm <sipos.miklos@nik.uni-obuda.hu>
2019-10-01 09:56:57 +0200

    added more attribute example

--- LA-04-reflexio/attributum/attributum/Program.cs
+++ LA-04-reflexio/attributum/attributum/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace attributum
 {
-
+    
     class HelpAttribute : Attribute
     {
         public string HelpText { get; set; }
@@ -17,13 +18,25 @@ namespace attributum
     // megszorítás >> csak osztályra helyezhető attrib.
 
     [AttributeUsage(AttributeTargets.Class)]
-    class MyClassAttribute : Attribute { }
+    class MyClassAttribute : Attribute
+    {
+        // ...
+    }
 
 
     // megszorítás >> csak metódusra helyezhető attrib.
 
     [AttributeUsage(AttributeTargets.Method)]
-    class MyMethodAttribute : Attribute { }
+    class MyMethodAttribute : Attribute
+    {
+        // ...
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    class CheckLength : Attribute
+    {
+        public int MaxLength { get; set; }
+    }
 
 
 
@@ -31,6 +44,7 @@ namespace attributum
     [MyClass] // ok
     class Auto
     {
+        [CheckLength(MaxLength = 7)]
         public string Rendszam { get; set; }
 
         [MyMethod] // ok
@@ -45,6 +59,11 @@ namespace attributum
 
     class Neptun
     {
+        /// <summary>
+        /// Visszaadja 'int' a jelenleg aktív hallgatók számát.
+        /// </summary>
+        public int AktivHallgatokSzama { get; set; }
+
         [Obsolete]
         //[Obsolete("használd az újabb metódust")] // >> megjelenik az üzenet a tooltip-ben
         //[Obsolete("használd az újabb metódust",true)] // >> errort kap, nem warningot
@@ -65,6 +84,28 @@ namespace attributum
 
     class Program
     {
+        static void RendszamBeallitas(Auto a, string r)
+        {
+            // feltételezve, hogy ez valahol egy nagy program szeparált pontján történik...
+            a.Rendszam = r;
+
+            // ellenőrzés
+            foreach (PropertyInfo p in a.GetType().GetProperties())
+            {
+                foreach (Attribute att in p.GetCustomAttributes())
+                {
+                    CheckLength c = (CheckLength)att;
+                    if (p.Name == "Rendszam")
+                    {
+                        if (a.Rendszam.Length > c.MaxLength) // hossz ellenőrzése
+                        {
+                            throw new Exception("A megadott érték túl hosszú.");
+                        }
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             Neptun n = new Neptun();
@@ -73,7 +114,22 @@ namespace attributum
 
 
 
+            // -----------------------------------------------------------------------------------------------------------
 
+
+            Auto a = new Auto();
+            try
+            {
+                RendszamBeallitas(a, "ASD-123-");
+            }
+            catch (Exception e )
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            
+            
+            
             // -----------------------------------------------------------------------------------------------------------
 
 
