ba7377c7cae1195a3ad89aacfba21ecdf6d0d55c
siposm <sipos.miklos@nik.uni-obuda.hu>
2019-10-08 09:48:18 +0200

    added dll files and class lib

--- LA-05-dll/egyszeru-pelda/CalcLib/CalcLib/Class1.cs
+++ LA-05-dll/egyszeru-pelda/CalcLib/CalcLib/Class1.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CalculatorLibrary
+namespace CalcLib
 {
     public class Calculator
     {
@@ -17,6 +13,5 @@ namespace CalculatorLibrary
         {
             return from - what;
         }
-
     }
 }
--- LA-05-dll/egyszeru-pelda/dllKezelo/dllKezelo/Program.cs
+++ LA-05-dll/egyszeru-pelda/dllKezelo/dllKezelo/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using CalculatorLibrary; // saját dll usingolása!
+using CalcLib; // saját dll usingolása!
 
 namespace dllKezelo
 {
