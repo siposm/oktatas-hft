3abc9e88c61515f970d8b9fbcb1b96aa7eaf3a04
siposm <sipos.miklos@nik.uni-obuda.hu>
2019-10-08 09:52:51 +0200

    added comments to CalcLib

--- LA-05-dll/egyszeru-pelda/CalcLib/CalcLib/Class1.cs
+++ LA-05-dll/egyszeru-pelda/CalcLib/CalcLib/Class1.cs
@@ -2,6 +2,18 @@
 
 namespace CalcLib
 {
+    // LÉPÉSEK
+    //
+    // 1. F6 >> BUILD SOLUTION
+    // 2. DLL FÁJL MEGKERESÉSE
+    // 3. DLL FÁJL BETALLÓZÁSA A MÁSIK PROJEKTBE (AHOL HASZNÁLNI AKARJUK)
+
+
+
+    /// <summary>
+    /// Calculator osztály, amellyel a .dll fájlok használata kerül bemutatásra.
+    /// Metódusai: Addition, Substraction
+    /// </summary>
     public class Calculator
     {
         public int Addition(int a, int b)
