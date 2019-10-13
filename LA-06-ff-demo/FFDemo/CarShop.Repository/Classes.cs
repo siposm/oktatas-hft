7374cea019c44288dd276587c806844773533a7e
siposm <sipos.miklos@nik.uni-obuda.hu>
2019-10-13 09:40:37 +0200

    added repo classes

---
+++ LA-06-ff-demo/FFDemo/CarShop.Repository/Classes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+using CarShop.Data;
+
+namespace CarShop.Repository
+{
+    public interface IRepository <T> where T : class
+    {
+        T GetOne(int id);
+        IQueryable<T> GetAll();
+    }
+
+    public interface ICarRepository : IRepository<cars> // using Carshop.Data-ból
+    {
+        void ChangePrice(int id, int newprice);
+    }
+
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected DbContext context; // using System.Data.Entity
+        public Repository(DbContext ctx)
+        {
+            this.context = ctx;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return context.Set<T>(); // set mint halmaz
+        }
+
+        public abstract T GetOne(int id);
+    }
+
+    public class CarRepository : Repository<cars>, ICarRepository
+    {
+        public CarRepository(DbContext ctx) : base(ctx) { }
+
+
+        public void ChangePrice(int id, int newprice)
+        {
+            var car = GetOne(id);
+            car.car_baseprice = newprice;
+            context.SaveChanges();
+        }
+
+        public override cars GetOne(int id)
+        {
+            return GetAll().SingleOrDefault(x => x.car_id == id);
+        }
+    }
+}
