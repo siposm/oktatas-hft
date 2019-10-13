202571944b657f6ff969446e10c8f8689f6b5906
siposm <sipos.miklos@nik.uni-obuda.hu>
2019-10-13 19:40:04 +0200

    added base files

---
+++ LA-06-ff-demo/FF-demo/FF-demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// teljes projekt hozzáadása a referenciákhoz
+using CarShop.Data;
+
+namespace FF_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // add connection string!
+            CarDatabaseEntities db = new CarDatabaseEntities();
+            Console.WriteLine(db.cars.Count());
+
+            
+        }
+    }
+}
