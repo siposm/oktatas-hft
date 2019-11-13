f87c2dc82a1627c8bf4b6d0cefdb8927fea82a58
siposm <sipos.miklos@nik.uni-obuda.hu>
2019-11-13 20:40:17 +0100

    added urls array

--- LA-09-process/parhuzamositas_process/01-chrome-basics/Program.cs
+++ LA-09-process/parhuzamositas_process/01-chrome-basics/Program.cs
@@ -13,14 +13,24 @@ namespace _01_chrome_basics
         {
             string url = "http://users.nik.uni-obuda.hu/siposm/";
 
-            for (int i = 0; i < 5; i++) // i = 1000 ? 
+            string[] urls = new string[]
+            {
+                "http://users.nik.uni-obuda.hu/sztf2/",
+                "http://users.nik.uni-obuda.hu/siposm/",
+                "http://users.nik.uni-obuda.hu/gitstats/",
+                "http://users.nik.uni-obuda.hu/prog3/",
+                "http://users.nik.uni-obuda.hu/to/",
+                "http://users.nik.uni-obuda.hu/prog4/"
+            };
+
+            for (int i = 0; i < urls.Length; i++) // i = 1000 ? 
             {
                 Process p = new Process();
                 p.StartInfo = new ProcessStartInfo();
                 p.StartInfo.FileName = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
                 // filename = url esetén is az alapértelmezett program nyílik meg
 
-                p.StartInfo.Arguments = url;
+                p.StartInfo.Arguments = urls[i];
                 p.Start();
 
                 //System.Threading.Thread.Sleep(1000);
