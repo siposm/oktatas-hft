20d9179ebad617dbb96ba44e39613a1d3d475e2c
siposm <sipos.miklos@nik.uni-obuda.hu>
2019-11-25 23:17:53 +0100

    added console runner example

--- LA-11-threadsync/threadSync/01-lock/Program.cs
+++ LA-11-threadsync/threadSync/01-lock/Program.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 
 namespace _01_lock
 {
+    
+    // * * * * * * * * * * * * *
+    //
+    //      VERSENYHELYZET
+    //
+    // * * * * * * * * * * * * *
+
     class Program
     {
         static int counter = 0;
@@ -43,7 +50,8 @@ namespace _01_lock
         {
             while (true)
             {
-                lock(threadLocker)
+                //A Lock is a way for us to synchronize between Threads. A lock is a shared object that can be Acquired by a Thread, and also Released. Once Acquired, other threads can be made to halt execution until the lock is Released.A lock is usually placed around a critical section, where you want to allow a single Thread at a time.
+                lock (threadLocker)
                 {
                     int _c = counter;
                     Thread.Sleep(250);
---
+++ LA-11-threadsync/threadSync/05-feladat-1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _05_feladat_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => RunConsole(ConsoleColor.Green, 0));
+            Task.Run(() => RunConsole(ConsoleColor.Blue, 2));
+            Task.Run(() => RunConsole(ConsoleColor.Yellow, 4));
+            Task.Run(() => RunConsole(ConsoleColor.Cyan, 6));
+            Console.ReadLine();
+        }
+
+        static Random rand = new Random();
+
+        static object lockObject = new object();
+
+
+        static void RunConsole(ConsoleColor c, int soridx)
+        {
+            for (int i = 0; i < 80; i++)
+            {
+                // vagy task.delay
+                Task.Delay(rand.Next(120, 750)).ContinueWith(x =>
+                {
+                    string s = ((char)rand.Next('a', 'z' + 1)).ToString();
+                    Draw(s, c, soridx, i);
+                }).Wait();
+
+                // vagy thread.sleep
+                //Thread.Sleep(rand.Next(120, 750));
+                //string s = ((char)rand.Next('a', 'z' + 1)).ToString();
+                //Draw(s, c, soridx, i);
+            }
+        }
+
+        static void Draw(string s, ConsoleColor c, int x, int y)
+        {
+            lock (lockObject)
+            {
+                Console.SetCursorPosition(y, x);
+                Console.ForegroundColor = c;
+                Console.Write(s);
+            }
+        }
+    }
+}
