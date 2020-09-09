b99f229adc03d64af1336464a56400744d66c5a8
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2020-09-09 17:32:38 +0200

    student class added

---
+++ LA-02-linq-xml/linq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace linq
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool Status { get; set; } /* eljegyezve / hajadon */
+
+        public override string ToString()
+        {
+            return Name + " " + Age + " " + Status;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
