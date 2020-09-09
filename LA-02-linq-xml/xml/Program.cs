a99b42dfe75d9bc779827506ccec1d0664cbd659
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2020-09-09 17:11:13 +0200

    task4-5 added

--- LA-02-linq-xml/xml/Program.cs
+++ LA-02-linq-xml/xml/Program.cs
@@ -64,6 +64,55 @@ namespace xml
 
             Process(task2);
 
+            // 3. feladat:
+            // számoljuk meg, hányan dolgoznak az egyes intézetekben
+
+            var task3 = from x in doc.Root.Descendants("person")
+                      group x by x.Element("dept").Value into g
+                      select new
+                      {
+                          Letszam = g.Count(),
+                          Intezet = g.Key
+                      };
+
+            Process(task3);
+
+            // 4. feladat
+            // számoljuk meg (egy teljesen új lekéréssel) hogy csak az AII-ben hányan
+            // HF: csináljuk meg ugyan ezt, de az eredmény a 3. feladat "intezetek" változójából nyerjük ki!
+
+            var task4 = from x in doc.Root.Descendants("person")
+                      where x.Element("dept").Value.Equals("Alkalmazott Informatikai Intézet")
+                      group x by x.Element("dept").Value into g
+                      select new
+                      {
+                          Letszam = g.Count(),
+                          Intezet = g.Key
+                      };
+
+            Process(task4);
+
+            // 5. feladat:
+            // adjunk hozzá az egész állományhoz egy új alkalmazottat
+
+
+            XElement uj = new XElement("person",
+                new XElement("name", "Ultron"),
+                new XElement("email", "ultron@gmail.com"),
+                new XElement("dept", "Alkalmazott Informatikai Intézet"),
+                new XElement("rank", "pusztító"),
+                new XElement("phone", "1234567890"),
+                new XElement("room", "BA.6.66")
+            );
+
+            doc.Root.Add(uj);
+
+            // Process(doc);
+
+            // mentés, ha akarjuk
+            //allomany.Save("ujTetszolegesNev.xml");
+
+
         }
     }
 }
