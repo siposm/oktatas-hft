1e95a4b5106c1488da11db45a39ad8f89ae042aa
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2020-09-11 11:29:04 +0200

    test files added

---
+++ TESZT/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace TESZT
+{
+    public class LibraryContext : DbContext
+  {
+    public DbSet<Book> Book { get; set; }
+
+    public DbSet<Publisher> Publisher { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseMySQL("server=localhost;database=library;user=user;password=password");
+      // UPDATE CONN. STRING !!
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity<Publisher>(entity =>
+      {
+        entity.HasKey(e => e.ID);
+        entity.Property(e => e.Name).IsRequired();
+      });
+
+      modelBuilder.Entity<Book>(entity =>
+      {
+        entity.HasKey(e => e.ISBN);
+        entity.Property(e => e.Title).IsRequired();
+        entity.HasOne(d => d.Publisher)
+          .WithMany(p => p.Books);
+      });
+    }
+  }
+  
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Language { get; set; }   
+        public int Pages { get; set; }
+        public virtual Publisher Publisher { get; set; }
+    }
+
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertData();
+            PrintData();
+        }
+
+        private static void InsertData()
+    {
+      using(var context = new LibraryContext())
+      {
+        // Creates the database if not exists
+        context.Database.EnsureCreated();
+
+        // Adds a publisher
+        var publisher = new Publisher
+        {
+          Name = "Mariner Books"
+        };
+        context.Publisher.Add(publisher);
+
+        // Adds some books
+        context.Book.Add(new Book
+        {
+          ISBN = "978-0544003415",
+          Title = "The Lord of the Rings",
+          Author = "J.R.R. Tolkien",
+          Language = "English",
+          Pages = 1216,
+          Publisher = publisher
+        });
+        context.Book.Add(new Book
+        {
+          ISBN = "978-0547247762",
+          Title = "The Sealed Letter",
+          Author = "Emma Donoghue",
+          Language = "English",
+          Pages = 416,
+          Publisher = publisher
+        });
+
+        // Saves changes
+        context.SaveChanges();
+      }
+    }
+
+    private static void PrintData()
+    {
+      // Gets and prints all books in database
+      using (var context = new LibraryContext())
+      {
+        var books = context.Book
+          .Include(p => p.Publisher);
+        foreach(var book in books)
+        {
+          var data = new StringBuilder();
+          data.AppendLine($"ISBN: {book.ISBN}");
+          data.AppendLine($"Title: {book.Title}");
+          data.AppendLine($"Publisher: {book.Publisher.Name}");
+          Console.WriteLine(data.ToString());
+        }
+      }
+    }
+    }
+}
