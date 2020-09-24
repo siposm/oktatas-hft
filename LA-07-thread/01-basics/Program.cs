7566ee393816849302f8270119f5fcd07fc34ab6
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2020-09-24 17:06:18 +0200

    added thread basics

---
+++ LA-07-thread/01-basics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace _01_basics
+{
+    class Program
+    {
+        static Random r = new Random();
+
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[10];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread( o => {
+                    int id = (int)o; // object
+            
+                    int min = r.Next(10);
+                    int max = r.Next(20, 30);
+
+                    for (int i = min; i < max; i++)
+                    {
+                        Console.WriteLine($"\t [{id}] : {i}");
+                    }
+                });
+                threads[i].Start(i);
+            }
+
+            // szinkronizációs pont // kommentezd ki és be, hogy lásd az eltérő működés vele / nélküle
+            for (int i = 0; i < threads.Length; i++)
+                threads[i].Join();
+
+            // szekvenciális rész innentől
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine("F I N I S H E D");
+
+        }
+    }
+}
