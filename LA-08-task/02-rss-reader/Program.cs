a754fcde986c86a4ab95c81b4c4eb3aa04167b2a
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2020-09-24 11:24:54 +0200

    base project added

---
+++ LA-08-task/02-rss-reader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_rss_reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
