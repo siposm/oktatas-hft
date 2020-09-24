fbfa86a00d78701c2d84d361d79e5a4cee501d8f
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2020-09-24 17:10:34 +0200

    added base files

---
+++ LA-07-thread/02-dataprocess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_dataprocess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
