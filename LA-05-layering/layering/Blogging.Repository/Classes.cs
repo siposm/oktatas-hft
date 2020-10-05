6938bc7095d3082db8a79aab9777e973a8364581
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-10-05 15:52:05 +0200

    added layering blog example

---
+++ LA-05-layering/layering/Blogging.Repository/Classes.cs
@@ -0,0 +1,44 @@
+﻿using Blogging.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Blogging.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected DbContext ctx;
+
+        public Repository(DbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return ctx.Set<T>(); // set => halmaz; != beállítás
+        }
+
+        public abstract T GetOne(int id);
+    }
+
+    public class BlogRepository : Repository<Blog>, IBlogRepository
+    {
+        public BlogRepository(DbContext ctx) : base(ctx) { /* ... */ }
+
+        public void ChangeTitle(int id, string newTitle)
+        {
+            var blog = GetOne(id);
+            blog.Title = newTitle;
+            ctx.SaveChanges();
+        }
+
+        public override Blog GetOne(int id)
+        {
+            return GetAll().SingleOrDefault(x => x.BlogId == id);
+        }
+    }
+}
---
+++ LA-05-layering/layering/layering/Program.cs
@@ -0,0 +1,68 @@
+﻿using Blogging.Data;
+using Blogging.Logic;
+using Blogging.Repository;
+using ConsoleTools;
+using System;
+using System.Linq;
+
+namespace layering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlogContext ctx = new BlogContext();
+            BlogRepository repo = new BlogRepository(ctx);
+            BlogLogic logic = new BlogLogic(repo);
+
+            var menu = new ConsoleMenu()
+                .Add(">> LIST ALL", () => ListAll(logic))
+                .Add(">> GET BY ID", () => GetById(logic))
+                .Add(">> CHANGE TITLE", () => ChangeTitle(logic))
+                .Add(">> EXIT", ConsoleMenu.Close);
+
+            menu.Show();
+        }
+
+        private static void ListAll(BlogLogic logic)
+        {
+            Console.WriteLine("\n:: ALL BLOGS ::\n");
+            logic.GetAllBlogs()
+                .ToList()
+                .ForEach(x => Console.WriteLine(x.MainData));
+
+            Console.ReadLine();
+        }
+
+        private static void GetById(BlogLogic logic)
+        {
+            Console.Write("ENTERT ID HERE: ");
+            int id = int.Parse(Console.ReadLine());
+
+            var q = logic.GetBlogById(id);
+
+            Console.WriteLine("\n:: SELECTED BLOG ::\n");
+            Console.WriteLine(q); // tostring
+
+            Console.ReadLine();
+        }
+
+        private static void ChangeTitle(BlogLogic logic)
+        {
+            Console.Write("ENTER ID HERE: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("ENTER NEW TITLE HERE: ");
+            string newTitle = Console.ReadLine();
+
+            logic.ChangeBlogTitle(id, newTitle);
+
+            var q = logic.GetBlogById(id);
+
+            Console.WriteLine("\n:: NEW TITLE ::\n");
+            Console.WriteLine(q.Title);
+
+            Console.ReadLine();
+        }
+    }
+}
