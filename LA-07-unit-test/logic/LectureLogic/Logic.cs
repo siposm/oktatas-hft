2ed2cc96a6b114ef6737d1536e8ec6ffb08cde4d
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2020-10-26 15:46:11 +0100

    methods added

--- LA-07-unit-test/logic/LectureLogic/Logic.cs
+++ LA-07-unit-test/logic/LectureLogic/Logic.cs
@@ -1,8 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LectureLogic
 {
-    public class Class1
+    /*
+    *   Note: model / entity class-okat nem szoktunk unit-tesztelni!
+    */
+
+    public class Lecture
     {
+        public string Name { get; set; }
+        public int HoursPerSemester { get; set; }
+        public int Credit { get; set; }
+        public List<Student> Students { get; set; } // dotnet add reference !!! a student-hez
+    }
+
+    public interface ILectureLogic
+    {
+        void AddLecture(Lecture lec);
+        Lecture GetLectureByIndex(int index);
+        void AddStudentToLecture(Student stud, int lectureIndex);
+    }
+
+    public class LectureLogic : ILectureLogic
+    {
+        private List<Lecture> Lectures { get; set; }
+
+        public LectureLogic()
+        {
+            this.Lectures = new List<Lecture>();
+        }
+
+        public void AddLecture(Lecture lec)
+        {
+            this.Lectures.Add(lec);
+        }
+
+        public Lecture GetLectureByIndex(int index)
+        {
+            return this.Lectures[index];
+        }
+
+        public void AddStudentToLecture(Student stud, int lectureIndex)
+        {
+            this.Lectures[lectureIndex].Students.Add(stud);
+        }
     }
 }
