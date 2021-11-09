2f32eafa4cc94e80d489c0ee0b2365d8beb38252
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2021-11-09 17:22:17 +0100

    task continuation basic example added

---
+++ LA-09-task/04-continuation-example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace tasktest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = new Task(() =>
+            {
+                Thread.Sleep(5000); // 5 sec wait
+                Console.WriteLine("[1] hello from the task!");
+            });
+
+            t.ContinueWith(t =>
+            {
+                Console.WriteLine("[2] hello from the continuation! :)");
+            });
+
+            t.Start();
+
+            Console.WriteLine("[0] some random lines after the 5 sec mark...");
+
+            Console.ReadLine();
+
+            // ReadLine is important otherwise the started task would end in the same way of course,
+            // but since the console's thread is closed (as the program reaches it's end)
+            // we would not see the console writeline neither of the task, nor of the contuniation!!!
+        }
+    }
+}
