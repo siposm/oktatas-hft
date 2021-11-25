55f541a808083b914be33c5b20fafbc4de4d36e4
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2021-11-25 15:58:14 +0100

    code updated

--- LA-10-task/05-cancellation-with-exception-example/Program.cs
+++ LA-10-task/05-cancellation-with-exception-example/Program.cs
@@ -12,16 +12,20 @@ namespace _05_cancellation_with_exception_example
             {
                 Console.WriteLine(i);
                 Thread.Sleep(100);
+
+                // comment in and out this if to see that the exception is not handled
+                if (i == 6)
+                    throw new IndexOutOfRangeException("Some exception which is NOT handled!");
             }
         }
 
         static void Main(string[] args)
         {
-            CancellationTokenSource source = new CancellationTokenSource();
+            CancellationTokenSource cts = new CancellationTokenSource();
 
-            Task taskWithCancel = Task.Run(() => CancellableLongOperation(source.Token), source.Token);
+            Task taskWithCancel = Task.Run(() => CancellableLongOperation(cts.Token), cts.Token);
 
-            source.Cancel(); // comment this in and out to see different outputs
+            //cts.Cancel(); // comment this in and out to see different outputs
 
             try
             {
@@ -31,7 +35,7 @@ namespace _05_cancellation_with_exception_example
             {
                 ae.Handle(e =>
                 {
-                    if (e is OperationCanceledException) // This we know how to handle.
+                    if (e is OperationCanceledException) // This is what we know how to handle.
                     {
                         Console.WriteLine("The operation was aborted.");
                         return true; // Mark as 'handled'
