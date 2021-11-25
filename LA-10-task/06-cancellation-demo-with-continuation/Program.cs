549e45f6e92b6283d924b50f89f72c62afcc6501
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2021-11-25 19:05:51 +0100

    task cancellation handling with continuation added

---
+++ LA-10-task/06-cancellation-demo-with-continuation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _06_cancellation_demo_with_continuation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Task task = Task.Run(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Thread.Sleep(300);
+                    Console.Write(i + "\t");
+
+                    if (cts.Token.IsCancellationRequested)
+                    {
+                        cts.Token.ThrowIfCancellationRequested();
+                    }
+                }
+            }, cts.Token);
+
+            task.ContinueWith(x =>
+            {
+                Console.WriteLine("TASK HAS BEEN CANCELED.");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+
+            Thread.Sleep(2000);
+            Console.WriteLine("Cancellation starting in 2 seconds...");
+            Thread.Sleep(2000);
+            cts.Cancel();
+            Console.WriteLine("Cancellation finished!");
+
+            Console.ReadLine();
+        }
+    }
+}
