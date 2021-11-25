d73a57d92fc2c98ddf91128fbc9c8466a4862f71
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2021-11-25 19:06:11 +0100

    cancellation handling with exception added

---
+++ LA-10-task/03-cancellation-demo-with-exception/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _03_cancellation_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Task task = Task.Run(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Thread.Sleep(300);
+                    Console.Write(i + "\t");
+
+                    if (cts.Token.IsCancellationRequested)
+                    {
+                        cts.Token.ThrowIfCancellationRequested();
+                    }
+                }
+            }, cts.Token);
+
+            cts.Cancel();
+
+            try
+            {
+                task.Wait();
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e =>
+                {
+                    if (e is OperationCanceledException) // This we know how to handle.
+                    {
+                        Console.WriteLine("The operation was canceled.");
+                        return true; // Mark as 'handled'
+                    }
+                    return false; // Let anything else stop the application.
+                });
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
