d090a36f46729a1a51a2eff31c73172a6277c6a2
Sipos Miklos <sipos.miklos@nik.uni-obuda.hu>
2021-11-27 11:42:19 +0100

    added lock

---
+++ LA-10-task/07-lock/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _07_lock
+{
+    class Program
+    {
+        static int sum = 0;
+        static object lockObject = new object();
+
+        static void Main(string[] args)
+        {
+            Task t1 = Task.Run(() => Count());
+            Task t2 = Task.Run(() => Count());
+
+            Task.WhenAll(t1, t2).ContinueWith(x =>
+             {
+                 Console.WriteLine("SUM: " + sum);
+             });
+
+            Console.ReadLine();
+        }
+
+        static void Count()
+        {
+            for (int i = 0; i < 50000; i++)
+            {
+                lock (lockObject)
+                {
+                    sum = sum + 1;
+                }
+            }
+        }
+    }
+}
