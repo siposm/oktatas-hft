2d750a6fa18e1aa7fd5ac686619132b164a77a05
siposm <commit@siposm.hu>
2023-09-19 22:49:31 +0200

    added dll and reflection example

---
+++ LA-03-reflection/04-dll-reflection/ReflectionPropertyHelper/ReflectionPropertyHelper/PropertyHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace ReflectionPropertyHelper
+{
+    public static class PropertyHelper
+    {
+        public static string GetPropertiesByAttribute<T>(T type, Attribute attribute)
+        {
+            string x = "";
+
+            foreach (var item in type.GetType().GetProperties()
+                .Where(x => x.GetCustomAttribute(attribute.GetType()) != null)
+            )
+            {
+                x += "\t";
+                x += item.Name + "\t=> ";
+                x += item.GetValue(type);
+                x += "\n";
+            }
+
+            return x;
+        }
+    }
+}
---
+++ LA-03-reflection/04-dll-reflection/classes-dll/classes-dll/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("classes-dll")]
+
+namespace classes_dll
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public Dog Dog { get; set; }
+    }
+    public class Dog
+    {
+        public string Name { get; set; }
+    }
+}
